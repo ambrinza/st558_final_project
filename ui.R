06af08ff087c537d194812deadaaaecc79b43e4f
Annie <annie@Annies-Air.attlocal.net>
2022-12-08 10:28:06 -0500

    Added in data and cleaned it

--- ui.R
+++ ui.R
@@ -1,5 +1,13 @@
 library(caret)
+library(tidyverse)
 library(shiny)
+df <- read.csv('data/Austin_Animal_Center_Outcomes.csv')
+
+# Cleaning the data before it will be used in the shiny app
+df <- df %>% 
+  filter(Name != "") %>% 
+  mutate(Name = gsub("\\*", "",Name)) %>%
+  separate(MonthYear, c("Month", "Year"))
 
 shinyUI(fluidPage(
   titlePanel(),
