02cf039dcff1b716c93e176631ac2f0d03251873
Annie <annie@Annies-MacBook-Air.local>
2022-12-11 11:48:04 -0500

    Added numerical summaries and bar plots

--- server.R
+++ server.R
@@ -5,10 +5,67 @@ library(tidyverse)
 df <- read.csv('data/Austin_Animal_Center_Outcomes.csv')
 
 # Cleaning the data before it will be used in the shiny app
+
 df <- df %>% 
-  filter(Name != "") %>% 
-  mutate(Name = gsub("\\*", "",Name)) %>%
-  separate(MonthYear, c("Month", "Year"))
+  mutate(Name = gsub("\\*", "",Name), 
+         Outcome.Type = ifelse(Outcome.Type == "Rto-Adopt", "Return to Owner", Outcome.Type),
+         Age = ifelse(grepl("month",Age.upon.Outcome), 0,as.numeric(sub(" .*", "", Age.upon.Outcome)))) %>%
+  separate(MonthYear, c("Month", "Year"))  %>%  filter(Name != "", Age >= 0, Outcome.Type != "",
+                                                       Sex.upon.Outcome != "NULL")
 
 shinyServer(function(input, output) {
+  # Create bar plot based on selection
+  output$bar_plot <- renderPlot({
+    # First, we want to filter to show only the n most popular for each group
+    df_counts <- df %>%
+          count(get(input$graph_options)) %>%
+            arrange(desc(n))
+    restricted_list <- df_counts[1:input$category_count,][1]
+    df_filtered <- df %>% filter(get(input$graph_options) %in% unlist(restricted_list))
+    if(input$grouping_options == "None"){
+      # Create  plain plot
+      g <- ggplot(df_filtered, aes_string(x = input$graph_options))
+      g + geom_bar()
+    } else{
+      # Create plot with grouping
+      # First need to filter the grouping var in the same way as above
+      df_counts <- df %>%
+        count(get(input$grouping_options)) %>%
+        arrange(desc(n))
+      restricted_list <- df_counts[1:input$category_count,][1]
+      df_filtered_group <- df_filtered %>% filter(get(input$grouping_options) %in% unlist(restricted_list))
+      g <- ggplot(df_filtered_group, aes_string(x = input$graph_options))
+      g + geom_bar(aes_string(fill = input$grouping_options), position = "dodge")
+    }
+  })
+  
+  output$histogram <- renderPlot({
+    g <- ggplot(df, aes(x = Age))
+    g + geom_histogram(binwidth = 1)
+  })
+    
+    
+  output$cont_table <- renderTable({
+    if(input$cont_table == 'One-Way'){
+      # Selecting the relevant column for a one-way contingency table
+      table(df %>% select(sym(input$one_way)))
+    } else {
+      first_col <-  df %>% select(sym(input$two_way_1))
+      second_col <- df %>% select(sym(input$two_way_2))
+      # table(first_col, second_col)
+      with(df, table(get(input$two_way_1),get(input$two_way_2)))
+    }
+  })
+  output$numeric_sum <- renderDataTable({
+    if(input$numeric_sum == 'Overall'){
+      df_numeric_sum <- df %>% summarise(mean = round(mean(Age), 2), median = median(Age), max = max(Age))
+    } else {
+      df_numeric_sum <- df %>% 
+        group_by(get(input$numeric_sum))%>%
+        summarise(mean = mean(Age), median = median(Age), max = max(Age))
+    }
+    
+  })
+    
+  
 })
--- ui.R
+++ ui.R
@@ -34,8 +34,58 @@ shinyUI(fluidPage(
              tabPanel("Data Exploration",
                       sidebarLayout(
                         sidebarPanel(
-                        ),
+                          h3("Select the types of summaries you would like to display"),
+                          selectInput("summary_choice", "Types of Summaries", 
+                                      choices = list("Bar Plot", "Histogram")),
+                          selectInput("table_choice", "Types of Tables",
+                                      choices = list("Contingency", "Numeric Summary")),
+                          # Only show this panel if the user selects "Bar Plot"
+                          conditionalPanel(condition = "input.summary_choice == 'Bar Plot'",
+                                           selectInput("graph_options", "What variable would you like to see?",
+                                                       choices = list("Outcome.Type", "Animal.Type", 
+                                                                      "Color", "Breed", "Sex.upon.Outcome")),
+                                           sliderInput("category_count", "Choose how many categories to see",
+                                                       value = 7, min = 3, max = 10),#FIXME to be dynamic UGH
+                                           selectInput("grouping_options", "What variable would you 
+                                                       like to color code by?", 
+                                                       choices = list("None","Outcome.Type", "Animal.Type", 
+                                                                       "Color", "Breed", "Sex.upon.Outcome"))), 
+                          conditionalPanel(condition = "input.table_choice == 'Numeric Summary",
+                                           selectInput("numeric_sum","How would you like to view 
+                                                       the Age variable?",
+                                                       choices = list("Overall", "Animal.Type", "Outcome.Type", 
+                                                                      "Outcome.Subtype", "Sex.upon.Outcome",
+                                                                      "Color", "Breed"))),
+                          conditionalPanel(condition = "input.table_choice == 'Contingency'",
+                                           radioButtons("cont_table", "What type of contingency table?",
+                                                       choices = list("One-Way", "Two-Way")),
+                          conditionalPanel(condition = "input.cont_table == 'One-Way'",
+                                           selectInput("one_way", "What variable would you 
+                                                       like to summarize?", 
+                                                       choices = list("Animal.Type", "Outcome.Type", 
+                                                                      "Outcome.Subtype", "Sex.upon.Outcome",
+                                                                      "Color", "Breed")),
+                                           ),
+                          conditionalPanel(condition = "input.cont_table == 'Two-Way'",
+                                           selectInput("two_way_1", "What is the first variable you 
+                                                       would like to summarize?", 
+                                                       choices = list("Outcome.Type", "Outcome.Subtype",
+                                                                      "Animal.Type", "Sex.upon.Outcome",
+                                                                      "Color", "Breed")),
+                                           selectInput("two_way_2", "What is the second variable you 
+                                                       would like to summarize?", 
+                                                       choices = list("Animal.Type", "Outcome.Type", 
+                                                                      "Outcome.Subtype", "Sex.upon.Outcome",
+                                                                      "Color", "Breed"))))
+                          ),
                         mainPanel(
+                          # Plot bar plot
+                          plotOutput("bar_plot"),
+                          # Plot summary table
+                          conditionalPanel(condition = "input.table_choice == 'Contingency'",
+                                          tableOutput("cont_table")),
+                          conditionalPanel(condition = "input.table_choice == 'Numeric Summary'",
+                                           dataTableOutput("numeric_sum"))
                         )
                       ) ),
              tabPanel("Modelling"),
@@ -49,4 +99,5 @@ shinyUI(fluidPage(
   #   mainPanel(
   #   )
   # )
+  
 ))
